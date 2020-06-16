d0b02d05cca18ba6851fce5ac2c847de6e2b8f5b
Ivan Laishevskiy <ivanlaish@ya.ru>
2020-06-17 01:01:51 +0300

    Реализованы функции банка - Получить состояние баланса - Удалить счет - Перевод со счета на счет

--- chaincode/hlf-course-chaincode-bank/bank-chaincode.go
+++ chaincode/hlf-course-chaincode-bank/bank-chaincode.go
@@ -9,7 +9,7 @@ import (
 )
 
 type BankAccount struct {
-	PersonID      uint64  `json:person_id`
+	PersonID      string  `json:person_id`
 	AccountNumber string  `json:account_number`
 	Balance       float64 `json:balance`
 }
@@ -51,6 +51,113 @@ var actions = map[string]func(stub shim.ChaincodeStubInterface, params []string)
 
 		return shim.Success(nil)
 	},
+
+	"getBalance": func(stub shim.ChaincodeStubInterface, params []string) peer.Response {
+		if len(params) != 1 {
+			return shim.Error(fmt.Sprintf("wrong number of parameters"))
+		}
+
+		state, err := stub.GetState(params[0])
+		if err != nil {
+			return shim.Error(fmt.Sprintf("failed to read bank account information with number %s, due to %s", params[0], err))
+		}
+
+		if state == nil {
+			return shim.Error(fmt.Sprintf("bank account with number %s doesn't exists", params[0]))
+		}
+		var account BankAccount
+		if err := json.Unmarshal([]byte(state), &account); err != nil {
+			return shim.Error(fmt.Sprintf("failed to read input %s, error %s", state, err))
+		}
+
+		return shim.Success(account.Balance)
+	},
+
+	"delAccount": func(stub shim.ChaincodeStubInterface, params []string) peer.Response {
+		if len(params) != 1 {
+			return shim.Error(fmt.Sprintf("wrong number of parameters"))
+		}
+
+		state, err := stub.GetState(params[0])
+		if err != nil {
+			return shim.Error(fmt.Sprintf("failed to read bank account information with number %s, due to %s", params[0], err))
+		}
+
+		if state == nil {
+			return shim.Error(fmt.Sprintf("bank account with number %s doesn't exists", params[0]))
+		}
+
+		err := stub.DelState(params[0])
+		if err != nil {
+			return shim.Error(fmt.Sprintf("failed to delete bank account information with number %s, due to %s", params[0], err))
+		}
+
+		return shim.Success(nil)
+	},
+
+	"transfer": func(stub shim.ChaincodeStubInterface, params []string) peer.Response {
+		var senderAccount BankAccount
+		var recipientAccount BankAccount
+		var amountToTransfer float64
+
+		if len(params) != 3 {
+			return shim.Error(fmt.Sprintf("wrong number of parameters. Usage: senderAccountNumber recipientAccountNumber amountToTransfer"))
+		}
+
+		if err := json.Unmarshal([]byte(params[2]), &amountToTransfer); err != nil {
+			return shim.Error(fmt.Sprintf("failed to read input %s, error %s", params[2], err))
+		}
+		
+		if params[0] != "0" {
+			senderState, err := stub.GetState(params[0])
+			if err != nil {
+				return shim.Error(fmt.Sprintf("failed to read sender bank account information with number %s, due to %s", params[0], err))
+			}
+
+			if senderState == nil {
+				return shim.Error(fmt.Sprintf("sender bank account with number %s doesn't exists", params[0]))
+			}
+			if err := json.Unmarshal([]byte(senderState), &account); err != nil {
+				return shim.Error(fmt.Sprintf("failed to read input %s, error %s", senderState, err))
+			}
+
+			if senderAccount.Balance-amountToTransfer < 0 {
+				return shim.Error(fmt.Sprintf("insufficient funds from the sender bank account with number %s. Sender has: %f , but want to transfer %f .", senderAccount.AccountNumber, senderAccount.Balance, amountToTransfer))
+			}
+		}
+
+		recipientState, err := stub.GetState(params[1])
+		if err != nil {
+			return shim.Error(fmt.Sprintf("failed to read recipient bank account information with number %s, due to %s", params[1], err))
+		}
+
+		if recipientState == nil {
+			return shim.Error(fmt.Sprintf("recipient bank account with number %s doesn't exists", params[1]))
+		}
+
+		if err := json.Unmarshal([]byte(recipientState), &recipientAccount); err != nil {
+			return shim.Error(fmt.Sprintf("failed to read input %s, error %s", recipientState, err))
+		}
+
+		if recipientAccount.Balance+amountToTransfer < recipientAccount.Balance {
+			return shim.Error(fmt.Sprintf("restricted operation with negative amount to transfer"))
+		}
+
+		senderAccount.Balance = senderAccount.Balance - amountToTransfer
+		recipientAccount.Balance = recipientAccount.Balance + amountToTransfer
+		senderBytes, err := json.Marshal(senderAccount)
+		recipientBytes, err := json.Marshal(recipientAccount)
+
+		if err := stub.PutState(senderAccount.AccountNumber, senderBytes); err != nil {
+			shim.Error(fmt.Sprintf("failed to save bank account with number %s, due to %s", senderAccount.AccountNumber, err))
+		}
+		if err := stub.PutState(recipientAccount.AccountNumber, recipientBytes); err != nil {
+			shim.Error(fmt.Sprintf("failed to save bank account with number %s, due to %s", recipientAccount.AccountNumber, err))
+		}
+
+		return shim.Success(nil)
+
+	}
 }
 
 func (b bankManagement) Init(stub shim.ChaincodeStubInterface) peer.Response {
